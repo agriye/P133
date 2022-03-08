f53b7f4fa16f2fe0e2a0dbadb6623abd85596fc0
SilverCOderZ <77063924+agriye@users.noreply.github.com>
2022-03-08 09:44:19 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,57 @@
+img = "";
+status = "";
+objects=[];
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status - Detecting the objects!";
+
+}
+function modelLoaded()
+{
+    console.log("Model is Loaded!");
+    status = true;
+    objectDetector.detect(img,gotResults);
+}
+function gotResults(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+   
+    console.log(results);
+   objects = results;
+}
+function preload()
+{
+    img = loadImage("Walking_tiger_female.jpg");
+}
+//!= means not equal to
+function draw()
+{
+    image(img,0,0,640,420);
+
+    if(status!="")
+{
+    for(i=0; i<objects.length; i++)
+    {
+        document.getElementById("status").innerHTML="Status : Objects Detected!";
+
+        fill("orangered");
+        percent = floor(objects[i].confidence*100);
+        text(objects[i].label + " : " + percent + "%", objects[i].x, objects[i].y);
+        noFill();
+        stroke("orangered");
+        rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+    }
+}
+    
+}
+function back()
+{
+    window.open("index.html");
+}
